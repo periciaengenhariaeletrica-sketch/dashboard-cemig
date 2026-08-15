aa76e94223c44620976916555ce73c09c8ac2d40
periciaengenhariaeletrica-sketch <pericia.engenhariaeletrica@gmail.com>
2026-08-15 13:16:15 -0300

    Atualiza sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cemig-dash-v1';
+const CACHE_NAME = 'cemig-dash-v1786810570';
 const ASSETS = [
   './',
   './index.html',
@@ -8,10 +8,11 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting())
+    })
   );
 });
 
@@ -19,7 +20,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys.map(k => caches.delete(k))
       );
     }).then(() => self.clients.claim())
   );
